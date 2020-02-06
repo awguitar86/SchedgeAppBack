b5b418da9e0562f1bc06e7d6f577dd485fefd7fd
Austin Wright <awguitar86@gmail.com>
2020-02-05 22:27:21 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const port = 8080;
+const {clearImage} = require('./util/file')
+
+const app = express();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'csvFiles');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + '-' + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'text/csv'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.use(bodyParser.json()); // application/json
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('file')
+);
+app.use('/csv', express.static(path.join(__dirname, 'csvFiles')));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+app.put('/post-image', (req, res, next) => {
+  // if (!req.isAuth) {
+  //   throw new Error('Not authenticated!');
+  // }
+  if (!req.file) {
+    return res.status(200).json({ message: 'No file provided!' });
+  }
+  if (req.body.oldPath) {
+    clearImage(req.body.oldPath);
+  }
+  return res
+    .status(201)
+    .json({ message: 'File stored.', filePath: req.file.path });
+});
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+app.listen(port, () => {
+  console.log(`=================================\nServer is listening on port ${port}.\n=================================`)
+})
